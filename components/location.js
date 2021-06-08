d37d7200d2e0a08a9de538c2d01f21a01e5c423c
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-06-07 23:08:48 -0400

    not working weather app :(

---
+++ components/location.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Location(props) {
+  return (
+    <div>
+      <h1>Hello User From {props.location} </h1>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,43 @@
 import { data } from "autoprefixer";
 import Head from "next/head";
+import Location from "../components/location";
+import { useState } from "react";
 
-export default function Home({data}) {
-  return <h1> {data.id} </h1>;
-}
-export async function getStaticProps() {
-  const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Markham&appid=${process.env.APP_ID}`
+export default function Home({ data }) {
+  const [userLocation, setuserLocation] = useState("");
+  const handleChange = (event) => {
+    setuserLocation(event.target.value);
+  };
+  const submission = async () => {
+    console.log(userLocation);
+    const info = getWeatherInfo(userLocation);
+  };
+  return (
+    <div>
+      <h1> Enter A Location </h1>
+      <form onSubmit={submission}>
+        <label> Location </label>
+        <textarea value={userLocation} onChange={handleChange} required />
+        <button type="submit">Get the weather!</button>
+      </form>
+    </div>
   );
-  const data = await res.json();
+}
 
-  if (!data) {
+async function getWeatherInfo(place) {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.APP_ID}`
+  );
+  const weatherData = await res.json();
+  console.log(props, "hi");
+  if (!weatherData) {
     return {
       notfound: true,
     };
   }
   return {
     props: {
-      data,
+      weatherData,
     },
   };
 }
